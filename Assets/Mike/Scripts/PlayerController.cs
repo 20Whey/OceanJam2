487773e818b6e0067cef0deb73420518d750f530
DrKend <michael.wood2002@Gmail.com>
2024-05-24 20:43:51 +0100

    playerspin

--- Assets/Mike/Scripts/PlayerController.cs
+++ Assets/Mike/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float playerHealth = 100f;
     public float playerDamage = 10f;
     public float playerAcceleration = 10f;
+    public float playerRotateSpeed = 0.3f;
+    public float dashBoost = 1.5f;
 
     private Vector2 moveDirection;
     private Rigidbody playerRB;
@@ -20,10 +22,31 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        RotatePlayer();
+        Dash();
+
     }
 
 	void FixedUpdate()
 	{
         playerRB.AddForce(moveDirection * playerAcceleration);
+	}
+
+    void RotatePlayer()
+	{
+        //Face Mouse
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotateAngle = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateAngle, playerRotateSpeed);
+    }
+
+    void Dash()
+	{
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerRB.AddForce(moveDirection * dashBoost, ForceMode.Impulse);
+        }
 	}
 }
