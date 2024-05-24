5fb03a9ae243d8673841965ca40032c263938939
Bababooey - aka Douglas Gaddum <DG246670@falmouth.ac.uk>
2024-05-24 19:19:49 +0100

    a

---
+++ Assets/Mike/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerHealth = 100f;
+    public float playerDamage = 10f;
+    public float playerAcceleration = 10f;
+
+    private Vector2 moveDirection;
+    private Rigidbody playerRB;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+    }
+
+	void FixedUpdate()
+	{
+        playerRB.AddForce(moveDirection * playerAcceleration);
+	}
+}
---
+++ Assets/Mike/Scripts/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+	public float playerHealth;
+	public float playerDamage;
+	public float playerMoveSpeed;
+}
