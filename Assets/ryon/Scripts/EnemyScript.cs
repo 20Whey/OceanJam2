da28877809d12c723f86903d666c5452b6ac5dcf
ryanevans48702 <ryan.evans-21@students.plymouth.ac.uk>
2024-05-25 16:07:32 +0100

    Health and Enemy Health

---
+++ Assets/ryon/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            TakeDamage(10); // Add 10 experience points when the space key is pressed
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
