6302a308a657ff01af8d8f14e687a9dbe672f09c
DrKend <michael.wood2002@Gmail.com>
2024-05-25 22:07:57 +0100

    vaccumfix

--- Assets/chlo/vaccumPickup.cs
+++ Assets/chlo/vaccumPickup.cs
@@ -1,69 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class vaccumPickup : MonoBehaviour
+public class VacuumPickup : MonoBehaviour
 {
-
     public float rad;
     [SerializeField] Collider[] nearbyColliders;
-    // Start is called before the first frame update
+    private HashSet<GameObject> collectedItems = new HashSet<GameObject>();
 
-    public IEnumerator moveToSucc(GameObject item)
+    public IEnumerator MoveToSucc(GameObject item)
     {
         var target = gameObject.transform;
         float elapsedTime = 0;
         float timeToMove = 5.0f;
+
         while (item.transform.position != target.position)
         {
             float t = elapsedTime / timeToMove;
-            item.transform.position = new Vector3(Mathf.Lerp(item.transform.position.x, target.position.x, t), Mathf.Lerp(item.transform.position.y, target.position.y, t), Mathf.Lerp(item.transform.position.z, target.position.z, t));
+            item.transform.position = new Vector3(
+                Mathf.Lerp(item.transform.position.x, target.position.x, t),
+                Mathf.Lerp(item.transform.position.y, target.position.y, t),
+                Mathf.Lerp(item.transform.position.z, target.position.z, t)
+            );
             elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
+
         Destroy(item);
     }
 
-    public int returnExperience(params Collider[] colliders)
+    public int ReturnExperience(params Collider[] colliders)
     {
         int val = 0;
+
         foreach (var item in colliders)
         {
-            bool bol = false;
+            if (collectedItems.Contains(item.gameObject)) continue;
+
+            bool isCollectible = false;
             GameObject gm = item.gameObject;
+
             switch (gm.tag)
             {
                 case "common":
                     val += 1;
-                    bol = true;
+                    isCollectible = true;
                     break;
-
                 case "rare":
-                    val = 2;
-                    bol = true;
+                    val += 2;
+                    isCollectible = true;
                     break;
-
                 case "epic":
-                    val = 5;
-                    bol = true;
+                    val += 5;
+                    isCollectible = true;
                     break;
             }
-            if (bol == true)
+
+            if (isCollectible)
             {
-                StartCoroutine(moveToSucc(gm));
+                collectedItems.Add(gm);
+                StartCoroutine(MoveToSucc(gm));
             }
         }
+
         return val;
     }
 
-
     void Update()
     {
-        nearbyColliders = Physics.OverlapSphere(transform.position, rad, 3);
-        if (nearbyColliders != null)
+        nearbyColliders = Physics.OverlapSphere(transform.position, rad);
+
+        if (nearbyColliders.Length > 0)
         {
-            GameObject.Find("Canvas").transform.GetChild(0).GetComponent<PlayerExp>().AddExperience(returnExperience(nearbyColliders));
+            int experienceToAdd = ReturnExperience(nearbyColliders);
+            GameObject.Find("Canvas").transform.GetChild(0).GetComponent<PlayerExp>().AddExperience(experienceToAdd);
         }
     }
 }
