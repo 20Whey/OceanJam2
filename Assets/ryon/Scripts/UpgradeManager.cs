87ed45b67eb39be09a6f28325f7abd53a8e373dd
ryanevans48702 <ryan.evans-21@students.plymouth.ac.uk>
2024-05-26 16:21:03 +0100

    Added infinite hashset no hashset

--- Assets/ryon/Scripts/UpgradeManager.cs
+++ Assets/ryon/Scripts/UpgradeManager.cs
@@ -36,6 +36,68 @@ public class UpgradeManager : MonoBehaviour
             case "MoveSpeed":
                 playerStats.playerMoveSpeed += upgrade.value;
                 break;
+            case "Barrier":
+                if (!playerStats.unlockedAbilities.Contains(PlayerAbilities.Shield))
+                {
+                    playerStats.unlockedAbilities.Add(PlayerAbilities.Shield);
+                    playerStats.playerShieldLevel += upgrade.value;
+                }
+                else
+                {
+                    playerStats.playerShieldLevel += upgrade.value;
+
+                }
+                break;
+            case "AttackSpeed":
+                playerStats.playerFireRate -= upgrade.value;
+                break;
+            case "ProjectileSpeedIncrease":
+                playerStats.playerProjectileSpeed += upgrade.value;
+                break;
+            case "SeekingMissile":
+                if (!playerStats.unlockedAbilities.Contains(PlayerAbilities.Missile))
+                {
+                    playerStats.unlockedAbilities.Add(PlayerAbilities.Missile);
+                    playerStats.playerMissileLevel += upgrade.value;
+                }
+                else
+                {
+                    playerStats.playerMissileLevel += upgrade.value;
+
+                }
+                break;
+            case "RotationSpeed":
+                playerStats.playerRotationSpeed += upgrade.value;
+                break;
+            case "DashLength":
+                playerStats.playerDashLength += upgrade.value;
+                break;
+            case "ViralSpinners":
+                if (!playerStats.unlockedAbilities.Contains(PlayerAbilities.Axes))
+                {
+                    playerStats.unlockedAbilities.Add(PlayerAbilities.Axes);
+
+                    playerStats.playerAxeLevel += upgrade.value;
+                }
+                else
+                {
+                    playerStats.playerAxeLevel += upgrade.value;
+
+                }
+                break;
+            case "Grabber":
+                if (!playerStats.unlockedAbilities.Contains(PlayerAbilities.Grab))
+                {
+                    playerStats.unlockedAbilities.Add(PlayerAbilities.Grab);
+
+                    playerStats.playerGrabLevel += upgrade.value;
+                }
+                else
+                {
+                    playerStats.playerGrabLevel += upgrade.value;
+
+                }
+                break;
 
         }
     }
